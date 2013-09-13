7ac650abf9ee7016ce04908d5e3bf8e7238a895f
Bing Han, Goh <stiucsib86@hotmail.com>
2013-09-13 23:24:05 +0800

    -n Initial dist subtree commit

---
+++ scripts/f96b343b.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("recyclefunWebApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("recyclefunWebApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
